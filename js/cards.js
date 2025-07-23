7d841eb6fe40985452219562e330e4a664f352b4
SergioBadaro <sergiobadaro@hotmail.com>
2025-07-23 13:30:22 -0300

    Novas atualizações e correções de bugs

--- js/cards.js
+++ js/cards.js
@@ -1,22 +1,27 @@
 // cards.js
-  function openModal(imgElement) {
-    const modal = document.getElementById('imageModal');
-    const modalImg = document.getElementById('modalImage');
 
-    modal.style.display = 'block';
-    modalImg.src = imgElement.src;
-    modalImg.alt = imgElement.alt;
+function openModal(imgElement) {
+  const modal = document.getElementById('imageModal');
+  const modalImg = document.getElementById('modalImage');
+
+  modalImg.src = imgElement.src;
+  modalImg.alt = imgElement.alt;
+
+  modal.classList.add('show');
+}
+
+function closeModal() {
+  const modal = document.getElementById('imageModal');
+  modal.classList.remove('show');
+}
+
+// Fecha o modal ao clicar fora da imagem
+window.addEventListener('click', function (event) {
+  const modal = document.getElementById('imageModal');
+  const modalImg = document.getElementById('modalImage');
+  
+  // Verifica se o clique foi fora da imagem dentro do modal
+  if (event.target === modal) {
+    closeModal();
   }
-
-  function closeModal() {
-    document.getElementById('imageModal').style.display = 'none';
-  }
-
-  // Fecha o modal ao clicar fora da imagem
-  window.onclick = function(event) {
-    const modal = document.getElementById('imageModal');
-    if (event.target === modal) {
-      modal.style.display = 'none';
-    }
-  }
-
+});
